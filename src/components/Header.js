003aa081e1a56c7ce50cf84d1c8f3da2e4e99129
kmh0831 <kmhyuk1018@gmail.com>
2024-08-01 11:55:18 +0900

    v3

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,19 +6,19 @@ const Header = () => {
       <div className="banner-top">
         <a href="/" className="logo">Airline Booking</a>
         <nav className="nav-links">
-          <a href="/signup">회원가입</a>
-          <a href="/login">로그인</a>
-          <a href="/support">고객센터</a>
-          <a href="/profile">나의 정보</a>
+          <a href="/hotels">호텔</a>
+          <a href="/flights">항공권</a>
+          <a href="/packages">항공+호텔</a>
+          <a href="/restaurants">유명 맛집</a>
+          <a href="/promotions">프로모션</a>
+          <a href="/guides">여행 가이드</a>
         </nav>
       </div>
       <div className="menu">
-        <a href="/hotels">호텔</a>
-        <a href="/flights">항공권</a>
-        <a href="/packages">항공+호텔</a>
-        <a href="/restaurants">유명 맛집</a>
-        <a href="/promotions">프로모션</a>
-        <a href="/guides">여행 가이드</a>
+        <a href="/signup">회원가입</a>
+        <a href="/login">로그인</a>
+        <a href="/support">고객센터</a>
+        <a href="/profile">나의 정보</a>
       </div>
     </header>
   );
